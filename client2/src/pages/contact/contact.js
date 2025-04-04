d36674c4382a2b3f0dabb96a024fb1d53e768c80
Allan-Binga <allanbinga73@gmail.com>
2025-04-04 06:15:40 -0400

    Navbar modifications

--- client2/src/pages/contact/contact.js
+++ client2/src/pages/contact/contact.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import TopHeader from '../../components/topHeader/TopHeader'
+import Header from '../../components/header/Header'
+import Footer from '../../components/footer/Footer'
 
 const Contact = () => {
   return (
-    <div>
-      
+    <div className="flex flex-col min-h-screen bg-gray-50">
+      <TopHeader />
+      <Header />
+      {/*Main Content*/}
+      <div className="flex-grow">
+      </div>
+      <Footer/>
     </div>
   )
 }
